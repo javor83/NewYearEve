f3fb398cb68f27f1ea4cd196ce7cc2ec9959da03
Явор Налбантов <javor.nalbantov@gmail.com>
2025-09-30 16:56:35 +0300

    ok

--- resx/view/resx_view_Index.cs
+++ resx/view/resx_view_Index.cs
@@ -3,5 +3,24 @@
     public class resx_view_Index
     {
         public const string Invite = "Изпрати покана";
+
+
+        public const string Label_Name = "Име";
+        
+        
+        public const string Label_Phone = "Телефон";
+       
+        
+        public const string Label_Email = "Електронна поща";
+        
+        
+        public const string Label_Attend = "Ще присъствам";
+
+        public const string Label_Accept = "Приемам";
+
+        public const string Label_Decline = "Отказвам";
+
+
+        public const string Label_NoInvite = "Няма покани";
     }
 }
